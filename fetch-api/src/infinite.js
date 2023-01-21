5ed8d5e1c9586a2a493915ba24a63f510f289ff0
amanaligit <ali.aman.2010@gmail.com>
2023-01-21 21:52:48 +0530

    infinite working

--- fetch-api/src/infinite.js
+++ fetch-api/src/infinite.js
@@ -1,60 +1,53 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-function Infinite(){
+function Infinite() {
 
-    const numberOfNewPhotos = 20;
+    const numberOfNewPhotos = 5;
 
     const [pageData, setPageData] = useState([]);
+    const [dataLength, setDataLenght] = useState(0);
 
-    // useEffect(() => {
-    function addMorePhotos(){
-        console.log('loading')
-        fetch('https://jsonplaceholder.typicode.com/photos')
-        .then(res => res.json())
-        .then(data => {
-            
-            const prevDataLength = pageData.length;
-            const newDataLength = prevDataLength + numberOfNewPhotos;
-            console.log(numberOfNewPhotos, prevDataLength);
-            if(newDataLength < 5000){
-                const newPageData = data.slice(0, newDataLength);
-                setPageData(newPageData);
-                console.log(newPageData.length)
-            }
-        })
+
+    function addMorePhotos() {
+        setDataLenght(d => d + numberOfNewPhotos)
     }
-    // }, [])
+
+    useEffect(() => {
+
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(res => res.json())
+            .then(data => {
+                const newPageData = data.slice(0, dataLength);
+                setPageData(newPageData);
+                window.addEventListener('scroll', runOnScroll);
+            })
+    }, [dataLength])
 
 
-    function runOnScroll(){
-        // at the end of scoll, window.scrollY+window.innerHeight === document.documentElement.scrollHeight
-        if(document.documentElement.scrollHeight - (window.scrollY+window.innerHeight) < 100){
+
+    function runOnScroll() {
+        if (document.documentElement.scrollHeight - (window.scrollY + window.innerHeight) < 1000) {
             // now load more photos:
+            window.removeEventListener('scroll', runOnScroll);
             console.log('user reached the end')
             addMorePhotos();
         }
-            
-        // console.log(document.documentElement.scrollHeight, window.scrollY, window.innerHeight);
-
     }
 
     useEffect(() => {
         addMorePhotos();
         window.addEventListener('scroll', runOnScroll)
-
+        return () => window.removeEventListener('scroll', runOnScroll);;
     }, [])
 
     return (
         <div>
             <h1 className='display-1 m-5'>Pagination Example</h1>
-
-
-
             <div className='container'>
                 {
                     pageData.map((data, i) =>
-                        <div key={i} className='card m-5' style={{width: '400px'}} >
+                        <div key={i} className='card m-5' style={{ width: '400px' }} >
                             <img src={data.url} alt='album data' className='card-img-top' />
                             <div className='card-body'>
                                 <p className='card-text display-6'>{data.title}</p>
